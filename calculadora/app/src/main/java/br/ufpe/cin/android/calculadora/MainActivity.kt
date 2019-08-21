aa5d063b2ee86afab6e42cd69ce3b9805f24ed0f
iuribtt <iuribtt@gmail.com>
2019-08-21 09:23:37 -0300

    Requisitos do exercício feitos

--- calculadora/app/src/main/java/br/ufpe/cin/android/calculadora/MainActivity.kt
+++ calculadora/app/src/main/java/br/ufpe/cin/android/calculadora/MainActivity.kt
@@ -1,15 +1,145 @@
 package br.ufpe.cin.android.calculadora
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        //salva os estados dos textos na tela
+        outState?.putCharSequence("text_calc", text_calc.text)
+        outState?.putCharSequence("text_info", text_info.text)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        //restaura os estados dos textos quando acontece uma mudanca de contexto
+        text_calc.setText(savedInstanceState?.getCharSequence("text_calc"))
+        text_info.setText(savedInstanceState?.getCharSequence("text_info"))
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Botão 0
+        btn_0.setOnClickListener { view ->
+            text_calc.setText(String.format("%s%s", text_calc.text.toString(), "0"))
+        }
+        //Botão 1
+        btn_1.setOnClickListener { view ->
+            text_calc.setText(String.format("%s%s", text_calc.text.toString(), "1"))
+        }
+
+        //Botão 2
+        btn_2.setOnClickListener { view ->
+            text_calc.setText(String.format("%s%s", text_calc.text.toString(), "2"))
+        }
+
+        //Botão 3
+        btn_3.setOnClickListener { view ->
+            text_calc.setText(String.format("%s%s", text_calc.text.toString(), "3"))
+        }
+        //Botão 4
+        btn_4.setOnClickListener { view ->
+            text_calc.setText(String.format("%s%s", text_calc.text.toString(), "4"))
+        }
+        //Botão 5
+        btn_5.setOnClickListener { view ->
+            text_calc.setText(String.format("%s%s", text_calc.text.toString(), "5"))
+        }
+        //Botão 6
+        btn_6.setOnClickListener { view ->
+            text_calc.setText(String.format("%s%s", text_calc.text.toString(), "6"))
+        }
+        //Botão 7
+        btn_7.setOnClickListener { view ->
+            text_calc.setText(String.format("%s%s", text_calc.text.toString(), "7"))
+        }
+        //Botão 8
+        btn_8.setOnClickListener { view ->
+            text_calc.setText(String.format("%s%s", text_calc.text.toString(), "8"))
+        }
+        //Botão 9
+        btn_9.setOnClickListener { view ->
+            text_calc.setText(String.format("%s%s", text_calc.text.toString(), "9"))
+        }
+
+        //Botão /
+        btn_Divide.setOnClickListener { view ->
+            text_calc.setText(String.format("%s%s", text_calc.text.toString(), "/"))
+        }
+
+        //Botão *
+        btn_Multiply.setOnClickListener { view ->
+            text_calc.setText(String.format("%s%s", text_calc.text.toString(), "*"))
+        }
+
+        //Botão -
+        btn_Subtract.setOnClickListener { view ->
+            text_calc.setText(String.format("%s%s", text_calc.text.toString(), "-"))
+        }
+
+        //Botão +
+        btn_Add.setOnClickListener { view ->
+            text_calc.setText(String.format("%s%s", text_calc.text.toString(), "+"))
+        }
+
+        //Botão (
+        btn_LParen.setOnClickListener { view ->
+            text_calc.setText(String.format("%s%s", text_calc.text.toString(), "("))
+        }
+
+        //Botão )
+        btn_RParen.setOnClickListener { view ->
+            text_calc.setText(String.format("%s%s", text_calc.text.toString(), ")"))
+        }
+
+        //Botão ^
+        btn_Power.setOnClickListener { view ->
+            text_calc.setText(String.format("%s%s", text_calc.text.toString(), "^"))
+        }
+
+        //Botão ^
+        btn_Power.setOnClickListener { view ->
+            text_calc.setText(String.format("%s%s", text_calc.text.toString(), "^"))
+        }
+
+        //Botão .
+        btn_Dot.setOnClickListener { view ->
+            text_calc.setText(String.format("%s%s", text_calc.text.toString(), "."))
+        }
+
+        //Botão =
+        btn_Equal.setOnClickListener { view ->
+
+            try {
+                text_info.text = eval(text_calc.text.toString()).toString()
+            } catch (e: RuntimeException) {
+                toast(this, "Expressão inválida")
+            }
+
+        }
+
+        //Botão C
+        btn_Clear.setOnClickListener { view ->
+            text_calc.setText("")
+        }
+
     }
 
+    /**
+     * Run toast method
+     */
+    fun Context.toast(context: Context = applicationContext, message: String, duration: Int = Toast.LENGTH_SHORT) {
+        Toast.makeText(context, message, duration).show()
+    }
 
     //Como usar a função:
     // eval("2+2") == 4.0
